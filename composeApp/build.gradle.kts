479ab7ada4f53c89374eb6078bb82d84fee0512d
Raphdf201 <raphdf201@hotmail.com>
2026-01-21 08:55:54 -0500

    Fix deprecation warnings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,15 +16,15 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.ui.tooling)
+            implementation(libs.compose.material.icons)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.compose.material.icons)
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.cio)
             implementation(libs.ktor.client.contentNegotiation)
